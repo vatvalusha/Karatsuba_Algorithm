6eaa35bd61342b8f6e4b7778c0c7defadbce3246
vatvalusha <vatvalusha@gmail.com>
2017-02-08 11:19:25 +0200

    commit

--- src/main/java/karatsuba/Main.java
+++ src/main/java/karatsuba/Main.java
@@ -12,41 +12,40 @@ import java.util.Scanner;
  * Created by vatva on 07.02.2017.
  */
 public class Main {
-    public static void main(String[] args)  throws Exception {
+    public static void main(String[] args) throws Exception {
 
         Karatsuba karatsuba = new Karatsuba();
         long start, stop;
+//        int var = 128;
         final double random = Math.random();
         Random rand = new Random();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(random);
-        System.out.println("1");
+//        System.out.println("Random for BigInteger: " + random);
 
-        int N = 2147483647 + (int)Math.round((random * 2147483647) + (-2147483648));
-//        BigInteger a = new BigInteger(N, random);
-        System.out.println(N);
-        System.out.println("Input number X: ");
-        BigInteger a = new BigInteger((int)  N, rand);
+
+        int N = 2048 + (int) (Math.random() * 4096);
+        System.out.println("Random for numBit: " + N);
+        BigInteger a = new BigInteger(N, rand);
         System.out.println("A: " + a);
-//        BigInteger b = new BigInteger(N, random);
-        System.out.println("Input number Y: ");
-        BigInteger b = new BigInteger(43, rand);
+        BigInteger b = new BigInteger(N, rand);
+        System.out.println("B: " + b);
 
-        System.out.println("B: "+ b);
-
+        System.out.println("Bit for A: " + a.bitLength());
+        System.out.println("Bit for B: " + b.bitLength());
         start = System.currentTimeMillis();
         BigInteger c = karatsuba.karatsuba(a, b);
-        System.out.println(c.toString());
+        System.out.println("Multiply: "+ c.toString());
         stop = System.currentTimeMillis();
         System.out.println("Time for Karatsuba multiply:");
         System.out.println(stop - start);
-
-        start = System.currentTimeMillis();
-        BigInteger d = a.multiply(b);
-        stop = System.currentTimeMillis();
-        System.out.println("Time for simple multiply:");
-        System.out.println(stop - start);
-
-        System.out.println((c.equals(d)));
+//
+//        start = System.currentTimeMillis();
+//        BigInteger d = a.multiply(b);
+//        stop = System.currentTimeMillis();
+//        System.out.println("Time for simple multiply:");
+//        System.out.println(stop - start);
+//        BigInteger result = karatsuba.karatsuba(new BigInteger("23"),new BigInteger("47"));
+//        System.out.println(result);
+//
+//        System.out.println((c.equals(d)));
     }
 }
